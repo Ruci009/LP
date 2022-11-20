89a681a6492885cb151591d05f2ee247f368ebff
Ruci009 <82047996+Ruci009@users.noreply.github.com>
2022-11-21 01:35:07 +0530

    Add files via upload

---
+++ fcfs.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int time, burst_time[10], at[10], sum_burst_time = 0, smallest, n, i;
+  int sumt = 0, sumw = 0;
+  cout<<"enter the no of processes : ";
+  cin>>n;
+  for (i = 0; i < n; i++) {
+    cout<<"the arrival time for process   "<< i + 1;
+    cin>>at[i];
+   cout<<"the burst time for process  "<< i + 1;
+   cin>>burst_time[i];
+    sum_burst_time += burst_time[i];
+  }
+  burst_time[9] = 9999;
+  for (time = 0; time < sum_burst_time;) {
+    smallest = 9;
+    for (i = 0; i < n; i++) {
+      if (at[i] <= time && burst_time[i] > 0 && burst_time[i] < burst_time[smallest])
+        smallest = i;
+    }
+   cout<< smallest + 1, time + burst_time[smallest] - at[smallest], time - at[smallest];
+    sumt += time + burst_time[smallest] - at[smallest];
+    sumw += time - at[smallest];
+    time += burst_time[smallest];
+    burst_time[smallest] = 0;
+  }
+  cout<<"\n\n average waiting time = "<< sumw * 1.0 / n;
+ cout<<"\n\n average turnaround time = "<< sumt * 1.0 / n;
+  return 0;
+}
+
